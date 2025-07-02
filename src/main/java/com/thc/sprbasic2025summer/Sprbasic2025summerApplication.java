35da9533ca48bccb6b594f1502a908594106104f
01 <talhwan@hotmail.com>
2025-07-02 10:26:05 +0900

    Initial commit

---
+++ src/main/java/com/thc/sprbasic2025summer/Sprbasic2025summerApplication.java
@@ -0,0 +1,13 @@
+package com.thc.sprbasic2025summer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sprbasic2025summerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sprbasic2025summerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/thc/sprbasic2025summer/Sprbasic2025summerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thc.sprbasic2025summer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sprbasic2025summerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
